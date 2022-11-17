62c3c5866946744892d036f00766fa87ab47ef41
Matt Hamann <mhamann@rownd.io>
2022-11-17 10:18:33 -0500

    chore: lint fixes

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -1,7 +1,7 @@
 import { DEFAULT_API_USER_AGENT } from '../src/lib/constants';
 
 describe('constants compile and resolve', () => {
-    it('user agent string compiles', () => {
-        expect(DEFAULT_API_USER_AGENT).toBeDefined();
-    })
+  it('user agent string compiles', () => {
+    expect(DEFAULT_API_USER_AGENT).toBeDefined();
+  });
 });
